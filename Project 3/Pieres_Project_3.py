29814187254cc44ef3f2bae623df126990927103
Hendrik <hendrik.pieres@googlemail.com>
2020-06-11 17:39:32 +0200

    Project 3: Pieres für Besprechung mit Dr. Kappe 12-6-20

--- Project 3/Pieres_Project_3.py
+++ Project 3/Pieres_Project_3.py
@@ -2,56 +2,31 @@ import pandas as pd
 import numpy as np
 import itertools
 import matplotlib.pyplot as plt
-from sklearn import linear_model
-from sklearn import svm
 from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import LogisticRegression
 
 df = pd.read_excel("WHR2018Chapter2OnlineData.xls")
 pd.options.display.max_columns = None
-
-def datensatzAnalyse():
-    global df
-    col = list(df.columns.values)
-
-    #GINI = statistisches Maß zum Aufzeigen von Ungleichverteilungen
-
-    print(col)
-    print(df.describe())
-
-def letztesJahr():
-    global df
-    print("Anzahl Länder:", len(list(df["country"].unique())))
-    print(df.loc[df["year"]==2017].sort_values("Life Ladder", ascending=False))
     
 def rankingLifeLadder(start: int):
-    #eigener Durchschnitt
-    pass
+    global df
+    data = df.loc[df["year"] >= start]
+    data = df.groupby("country").mean().sort_values("Life Ladder", ascending = False)
+    print(data)
 
-def ranking():
-    df = pd.read_excel("WHR2018Chapter2OnlineData.xls", sheet_name = "Figure2.2")
-    col = list(df.columns.values)
-    df = df.drop(columns=col[11:])
-    #Es wird eine Zeile zu viel eingelesen
-    df = df.dropna()
-    print(df.sort_values("Happiness score", ascending = False))
-    plt.barh(np.array(df["Country"]), np.array(df["Happiness score"]))
-    plt.show()
-    
 def entwicklung(country: list):
     global df
     lr = LinearRegression()
+    xp = np.linspace(2005,2017,24)
+    color = ["blue", "green", "red", "yellow", "black"]
     #LogisticRegression funktioniert nur mit Float-Werten?
-    lor = LogisticRegression()
-    for land in country:
+    for index, land in enumerate(country):
         data = df.loc[df["country"] == land]
-        plt.scatter(data["year"], data["Life Ladder"])
-        jahr = np.array(data["year"]).reshape(-1,1)
-        ladder = np.array(data["Life Ladder"]).reshape(-1,1)
-        lr.fit(jahr, ladder)
-        #lor.fit(jahr, ladder)
-        plt.plot(data["year"], lr.predict(jahr), label = land)
-        #plt.plot(data["year"], lor.predict(jahr.append(c for c in range(2018,2022))), label = land)
+        jahr = np.array(data["year"])
+        ladder = np.array(data["Life Ladder"])
+        lr.fit(jahr.reshape(-1,1), ladder.reshape(-1,1))
+        plt.plot(data["year"], lr.predict(jahr.reshape(-1,1)), c=color[index], label = land)
+        p = np.poly1d(np.polyfit(jahr, ladder, 3))
+        plt.plot(jahr, ladder, "o", xp, p(xp), c=color[index])
     plt.legend(loc="best")
     plt.show()
 
@@ -59,8 +34,8 @@ def continents():
     df = pd.read_excel("WHR2018Chapter2OnlineData.xls", sheet_name = "SupportingFactors")
     data = df[["Region indicator", "Life ladder, 2015-2017"]]
     data = data.groupby("Region indicator").mean().sort_values("Life ladder, 2015-2017", ascending=False)
-    print(data)
-    
+    print(data)    
+
 def einzel():
     df = pd.read_excel("WHR2018Chapter2OnlineData.xls", sheet_name = "Figure2.2")
     col = list(df.columns.values)
@@ -69,6 +44,7 @@ def einzel():
     factors = ["GDP", "Social Support", "Healthy Life", "Freedom", "Generosity", "Corruption"]
     facCoun = {}
     length = 0
+        
     for index, key in enumerate(factors, 4):
         #Interpretation von Perception of corruption??
         asc = False if index < 9 else True
@@ -76,13 +52,15 @@ def einzel():
         dataTemp.reset_index(drop = True, inplace = True)
         loes = dataTemp["Country"][0]
         facCoun[key] = loes
-        if len(loes) > len(key):
-            length += len(loes)
-        else:
-            length += len(key)
-        keys = list(facCoun.keys())
-    print("".join(c for c in itertools.repeat("#", length + 1 + 3*len(factors))))
-    print("# ", end="")
+        length = length + len(loes) if len(loes) > len(key) else length + len(key)
+    keys = list(facCoun.keys())
+        
+    def trennlinie(fertig = False):
+        print("".join(c for c in itertools.repeat("#", length + 1 + 3*len(factors))))
+        if fertig == False:
+            print("# ", end ="")
+            
+    trennlinie()
     for key in facCoun:
         if len(facCoun[key]) > len(key):
             zusatz = "".join(c for c in itertools.repeat(" ", len(facCoun[key]) - len(key)))
@@ -90,8 +68,7 @@ def einzel():
             zusatz=""
         print(str(key + zusatz), "| ", end = "") if key != keys[-1] else print(key + zusatz, end="")
     print(" #")
-    print("".join(c for c in itertools.repeat("#", length + 1 + 3*len(factors))))
-    print("# ", end="")
+    trennlinie()
     for key in facCoun:
         if len(key) > len(facCoun[key]):
             zusatz = "".join(c for c in itertools.repeat(" ", len(key) - len(facCoun[key])))
@@ -99,7 +76,7 @@ def einzel():
             zusatz=""
         print(str(facCoun[key] + zusatz), "| ", end ="") if key != keys[-1] else print(str(facCoun[key] + zusatz), end="")
     print(" #")
-    print("".join(c for c in itertools.repeat("#", length + 1 + 3*len(factors))))
+    trennlinie(True)
     
 def corruption():
     global df
@@ -125,18 +102,15 @@ def rankingDetail():
             daten.append(dfTemp[spalte])
         left=0
         for i, (data, color) in enumerate(zip(daten, colorList),4):
-            #Platzierung wäre noch gut
             xerr = dfTemp.iloc[:,2]-dfTemp.iloc[:,3] if i == 10 else None
-            plt.barh(dfTemp["Country"], data, color=color, left=left, xerr=xerr, label = col[i])
+            ar = np.char.mod('%d', dfTemp.index.values + 1)
+            dfTemp["Label"] = [value + '. ' + c + '(' + str(round(h,3)) + ')' for value, c, h in zip(ar, dfTemp["Country"], dfTemp["Happiness score"])] 
+            #stac = True -> erspart For-Schleife
+            plt.barh(dfTemp["Label"], data, color=color, left=left, xerr=xerr, label = col[i])
             plt.gca().invert_yaxis()
             plt.legend(ncol = int(len(colorList)/2), bbox_to_anchor=(0,1), loc="lower left", fontsize="small")
             left+=data
         plt.show()
-        
-def noData():
-    global df
-    data = df.groupby("country").mean()
-    print(data.isnull().sum())
 
 def whereToGo():
     df = pd.read_excel("WHR2018Chapter2OnlineData.xls", sheet_name = "Figure2.4")
@@ -146,14 +120,31 @@ def whereToGo():
     col = list(map(lambda x: str.replace(x, "Average happiness of", "happiness"), col))
     df.columns = col
     df["Diff"] = df.iloc[:,1] - df.iloc[:,4]
+    df = df.loc[((df[col[1]] + df[col[-2]])/2) > 6.5]
     df = df.sort_values("Diff", ascending = False)
     print(df.head())
 
+def entwicklungV2(country: list):
+    global df
+    color = ["blue", "green", "red", "yellow", "black"]
+    for index, land in enumerate(country):
+        data = df.loc[df["country"] == land]
+        xp = np.linspace(2005, 2020, 30)
+        jahr = np.array(data["year"])
+        ladder = np.array(data["Life Ladder"])
+        plt.scatter(jahr, ladder, c=color[index])
+        for i in range(1,4):
+            p = np.poly1d(np.polyfit(jahr, ladder, i))
+            plt.plot(jahr, ladder, "o", xp, p(xp), c=color[index])
+    plt.show()
+
 def main():
     entwicklung(["Germany", "Finland", "Denmark"])
+    #entwicklungV2(["Germany", "Finland"])
     #rankingDetail()
-    #einzel()
-    whereToGo()
+    einzel()
+    #whereToGo()
+    #rankingLifeLadder(2013)
     
 if __name__ == "__main__":
     main()
