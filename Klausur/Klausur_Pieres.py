be535eba9964ea53c8052ba510d8f37cd2ec7f37
Hendrik <hendrik.pieres@googlemail.com>
2020-07-04 13:56:15 +0200

    V1.2

--- Klausur/Klausur_Pieres.py
+++ Klausur/Klausur_Pieres.py
@@ -2,14 +2,17 @@ import pandas as pd
 import itertools
 import numpy as np
 import matplotlib.pyplot as plt
+import re
 
 pm = pd.read_csv("aap_air_quality_database_2018_v14.csv", skiprows=2, sep=";")
 pd.options.display.max_columns = None
 
-#Aufteilung in zwei unterschiedliche DataFrames, damit Python nicht mit den gleichen
-#Spaltennamen durcheinander kommt
+#Aufteilung in zwei unterschiedliche DataFrames
 pm10 = pm.drop(columns=pm.iloc[:,8:11])
 pm25 = pm.drop(columns=pm.iloc[:,5:8])
+
+#im Folgenden wird häufig col[5] verwendet -> damit wird die Spalte der jährlichen 
+#Werte ausgewählt
 
 def zwischenlinie(length: int):
     print("".join(c for c in itertools.repeat("#", length + 10)))
@@ -24,6 +27,7 @@ def einkommensVergleich():
     pm = pm.drop(columns = pm.iloc[:,5:8])
     col = list(pm.columns.values)
     pm25Gr = 25
+    pm["Limit"] = pm[col[5]] <= pm25Gr
     income = ["LMIC", "HIC"]
     #Die Kontinente ggf. über replace HIC, LMIC in Python ermitteln lassen -> Vermeidung von Tippfehlern
     continents = ["Africa", "Americas", "Eastern Mediterranean", "Europe", "South-East Asia", "Western Pacific"]
@@ -48,12 +52,16 @@ def einkommensVergleich():
         data = pm.loc[pm["Region"].str.contains(con)]
         format = "# %%%ds" % maxCon
         print(format % con, end = " ")
-        for index, ein in enumerate(income):
+        for ein in income:
             dataTemp = data.loc[data["Region"].str.contains(ein)]
+            
+            #Prüfen, ob der DataFrame überhaupt Daten enthält (gibt es HIC-Städte,
+            #im jeweiligen Kontinent im Datensatz)
             if dataTemp.Region.count() > 0:
-                e = dataTemp.loc[dataTemp[col[5]] <= pm25Gr]
-                p = round(e.Region.count()/dataTemp.Region.count()*100,2)
-                if index == 0:
+                p = round((dataTemp.Limit == True).sum()/dataTemp.Region.count()*100,2)
+                
+                #Die Raute am Ende muss nur bei HIC (letzte Spalte) eingefügt werden
+                if ein == "LMIC":
                     print("# %5.2f" % p, end = " ")
                 else:
                     format = "# %%%d.2f #" % lenNoV
@@ -73,6 +81,9 @@ def stadtEntwicklung(stadt: str):
     for index, df in enumerate(frames):
         col = list(df.columns.values)
         data = df.loc[df["City/Town"] == stadt]
+        
+        #Die Regression soll nur durchgeführt werden, wenn mehr als zwei Datensätze
+        #vorhanden sind.
         if data["Year"].count() > 2:
             xp = np.linspace(min(data["Year"]), max(data["Year"]), 100)
             anMean = np.array(data[col[5]])
@@ -88,6 +99,22 @@ def stadtEntwicklung(stadt: str):
     plt.show()
 
 def stadtRanking(country: str, asc = True):
+    r"""
+    
+
+    Parameters
+    ----------
+    country : str
+        Land, für welches das Städteranking durchgeführt werden soll.
+    asc : TYPE, optional
+        Ob der DataFrame auf- (True) bzw. absteigend (False) ausgegeben werden soll. 
+        The default is True.
+
+    Returns
+    -------
+    None.
+
+    """
     global pm10
     global pm25
     
@@ -100,6 +127,13 @@ def stadtRanking(country: str, asc = True):
         data = data.loc[:, ["City/Town", col[5]]]
         print("Top 10 Ranking",key,"(Best):") if asc == True else print("Top 10 Ranking",key,"(Worst):")
         print(data.head(10), "\n")
+
+def GetStationCount(value: str)->int:
+    count = 0
+    strvalue = str(value)
+    for match in re.findall(r'(\d+)\s+\D+', strvalue):
+        count += int(match)
+    return count
 
 def aufbereitung():
     
@@ -121,7 +155,8 @@ def aufbereitung():
         temp = list(df[col[6]].unique())
         temp.sort()
         for index, cover in enumerate(temp):
-            df[col[6]].replace(cover, index, inplace = True)        
+            df[col[6]].replace(cover, index, inplace = True)   
+        df[col[-4]] = [GetStationCount(x) for x in df[col[-4]]]
 
 def main():
     aufbereitung()
