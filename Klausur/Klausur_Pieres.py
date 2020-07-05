d001f0d9f5cd1931a15748f013b73cd75c822764
Hendrik <hendrik.pieres@googlemail.com>
2020-07-05 20:00:06 +0200

    Funktionen auskommentiert

--- Klausur/Klausur_Pieres.py
+++ Klausur/Klausur_Pieres.py
@@ -24,22 +24,52 @@ pm25_latest = pm.drop(columns = pm.iloc[:,5:8])
 #feststehende Grenzwerte 
 PMLimits={"PM10": [40, pm10_latest], "PM25": [25, pm25_latest]}
 
-#im Folgenden wird häufig col[5] verwendet -> damit wird die Spalte der jährlichen 
-#Werte ausgewählt
-
 def zwischenlinie(length: int):
+    r"""
+    
+
+    Parameters
+    ----------
+    length : int
+        Wie viele Zeichen beinhalten die längste Werte in den jeweiligen Spalten zusammengerechnet.
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    #Der Wert 10 ergibt sich aus den Leerzeichen am Anfang und am Ende jeder Spalte (3*2) und den 
+    #Rauten zum Trennen sowie am Anfang und Ende (4*1)
     print("".join(c for c in itertools.repeat("#", length + 10)))
 
 def einkommensVergleich(df:pd.DataFrame, limit:int, df_info:str):
+    r"""
+    
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Datensatz, auf dem der Vergleich, wie viele Städte prozentual den Maximal-Wert 
+        der Luftverschmutzung übersteigen.
+    limit : int
+        Je nachdem, welche Partikelmasse betrachtet wird, wird ein unterschiedlicher
+        Maximal-Wert vorausgesetzt.
+    df_info : str
+        Wird lediglich als Extra-Parameter übergeben, um diesen in den Titel zu übernehmen.
+
+    Returns
+    -------
+    None.
+
+    """
     
     
     print(str("Prozentualer Anteil der Städte, die die \nWHO-Grenzwerte einhalten (" + df_info +")\n" ))
     
     df["limit"] = df.annual_mean <= limit
-    #print(df)
     HighIncome = [False,True]
-    #Die Kontinente ggf. über replace HIC, LMIC in Python ermitteln lassen -> Vermeidung von Tippfehlern
-    continents = ["Africa", "Americas", "Eastern Mediterranean", "Europe", "South-East Asia", "Western Pacific"]
+    continents = df.region.unique()
     
     #Länge der längsten Strings ermitteln, um Zwischenlinie zu skalieren
     maxCon = max([len(x) for x in continents])
@@ -59,7 +89,6 @@ def einkommensVergleich(df:pd.DataFrame, limit:int, df_info:str):
     #ausgewertet und gleichzeitig ausgegeben.
     for con in continents:
         data = df.loc[df.region.str.contains(con)]
-        #print(data.Region.count())
         format = "# %%%ds" % maxCon
         print(format % con, end = " ")
         
@@ -86,21 +115,43 @@ def einkommensVergleich(df:pd.DataFrame, limit:int, df_info:str):
 
     
 def stadtEntwicklung(stadt: str):
+    r"""
+    
+
+    Parameters
+    ----------
+    stadt : str
+        Stadt, für die die Entwicklung über die letzten Jahre über die Datenpunkte
+        inkl. einer Regressionsgraden angezeigt werden soll. 
+        Voraussetzung: Mehr als 2 Dateneinträge.
+
+    Returns
+    -------
+    None.
+
+    """
     global pm10
     global pm25
     
     frames = [pm10, pm25]
     color = ["blue", "red"]
     for index, df in enumerate(frames):
-        #col = list(df.columns.values)
         data = df.loc[df["city"] == stadt]
         
         #Die Regression soll nur durchgeführt werden, wenn mehr als zwei Datensätze
         #vorhanden sind.
         if data["year"].count() > 2:
+            
+            #Hundert gleichverteilte Werte zur Regressionsberechnung
             xp = np.linspace(min(data["year"]), max(data["year"]), 100)
+            
+            #Ermittlung der Luft-Verschmutzungswerte
             anMean = np.array(data.annual_mean)
+            
+            #Berechnung der Regressionsgraden
             p = np.poly1d(np.polyfit(data["year"], anMean, 1))
+            
+            #Visuelle Darstellung
             plt.plot(data["year"], anMean, "o")
             plt.plot(xp, p(xp), c = color[index], label=stadt)
         elif data["year"].count() == 0:
@@ -137,19 +188,30 @@ def stadtRanking(country: str, asc = True):
     frames = {"PM10": pm10, "PM2.5": pm25}
     for key in frames:
         df = frames[key]
-        #col = list(df.columns.values)
         data = df.loc[df["year"] == 2016]
         data = data.loc[data["country"] == country].sort_values("annual_mean", ascending=asc)
         data = data.loc[:, ["city", "annual_mean"]]
-        print("Top 10 Ranking",key,"(Best):") if asc == True else print("Top 10 Ranking",key,"(Worst):")
-        print(data.head(10), "\n")
         plt.barh(data.head(10).city, data.head(10).annual_mean)    
         plt.title(str("Top 10 Ranking Cities in " +country + " " +key+" (Best):"))  if asc == True else plt.title(str("Top 10 Ranking Cities in " +country+ " " +key+" (Worst):"))    
         plt.gca().invert_yaxis()
         plt.show()
-       # plt.barh(data.head(10).annual_mean,width=5)
 
 def GetStationCount(value: str)->int:
+    r"""
+    
+
+    Parameters
+    ----------
+    value : str
+        Jeweiliger Pandas-Dateneintrag.
+        Funktion zum Bereinigen und Addieren der Messstationen einer Stadt.
+
+    Returns
+    -------
+    int
+        Anzahl der Messstationen in der Stadt.
+
+    """
     count = 0
     strvalue = str(value)
     for match in re.findall(r'(\d+)\s+\D+', strvalue):
@@ -157,12 +219,20 @@ def GetStationCount(value: str)->int:
     return count
 
 def aufbereitung():
+    r"""
     
-    #Zur einfacheren Handhabung werden die umgerechneten Zahlen gleichbetrachtet
-    #Temporal coverage wird in Klassen aufgeteilt
-    
-    #ggf. weitere Spalten aufgrund von Nichtbetrachtung droppen
-    #ggf. noch dropna    
+    In dieser Methode werden die Datensätze gleich bereinigt.
+    Es wird nicht berücksichtigt, ob die Werte berechnet oder gemessen wurden.
+    Der Wert "temporal coverage" wurde durch eine Klassifikation ersetzt.
+    Das Einkommen der Stadt sowie die Angabe, ob es sich um einen berechneten oder einen
+    gemessenen Wert handelt, wurden als binäre Werte in eine separate Spalte aufgenommen bzw.
+    umgewandelt.
+
+    Returns
+    -------
+    None.
+
+    """
     
     cols = ['region','iso3','country','city','year','annual_mean','temp_coverage','measured','monitor_station_count','reference','db','status','HIC']
     global pm10
@@ -184,7 +254,7 @@ def aufbereitung():
         #Bereinigen von Annual mean --> nur noch Wert
         df.annual_mean.replace(r'\D', '', regex = True, inplace = True)
         df.annual_mean= df.annual_mean.astype(int)
-        #Bereinigen von temo coverage --> 
+        #Bereinigen von temp coverage --> 
         df.temp_coverage = df.temp_coverage.fillna(0)
         df.temp_coverage= df.temp_coverage.astype(str)
         temp = list(df.temp_coverage.unique())
